359dcd142e91bc3e3db123eba01c732e7a684db7
Nate Marti <nate_marti@sil.org>
2022-01-21 10:13:47 +0100

    fix manpage installation

--- tests/manpage.sh
+++ tests/manpage.sh
@@ -8,11 +8,14 @@ tests_dir=$(realpath $(dirname "${0}"))
 root_dir=$(dirname "$tests_dir")
 app_name=$(basename "$root_dir")
 draft="${root_dir}/data/man/${app_name}.1.md"
-outfile="${draft%.*}"
-outfile="${outfile##*/}"
-outfile="${root_dir}/debian/${outfile}"
+outfile_name="${draft%.*}"
+outfile_name="${outfile_name##*/}"
+outfile="${root_dir}/doc/${outfile_name}"
+debfile="${root_dir}/debian/${app_name}.manpages"
 if [[ -z "$1" || "$1" == 'preview' || "$1" == 'p' ]]; then
     pandoc "$draft" -s -t man | man -l -
 elif [[ "$1" == 'convert' || "$1" == 'c' ]]; then
+    mkdir -p "${root_dir}/doc"
     pandoc "$draft" -s -t man -o "$outfile"
+    echo "doc/${outfile_name}" > "$debfile"
 fi
