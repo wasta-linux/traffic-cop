22c5ac3f051ccd74cddeabcf3bc8b1e6037f2095
Nate Marti <nate_marti@sil.org>
2023-11-28 17:50:29 +0100

    clean out commented code

--- setup.py
+++ setup.py
@@ -4,30 +4,13 @@ import glob
 from setuptools import setup
 
 deb_pkg = 'traffic-cop'
-# py3_pkg = 'trafficcop'
-
-# # Get version from trafficcop.config.VERSION.
-# with open('trafficcop/config.py') as f:
-#     for line in f:
-#         if line.startswith('VERSION'):
-#             version = line.split('=')[1].replace("'", "").strip()
-#             break
 
 setup(
-    # name='Traffic Cop',
-    # version=version,
-    # description="Manage bandwidth usage by app or process.",
-    # author="Nate Marti",
-    # author_email="nate_marti@sil.org",
-    # url=f"https://github.com/wasta-linux/{deb_pkg}",
-    # packages=[py3_pkg],
-    # package_data={py3_pkg: ['README.md']},
     scripts=[
-        # 'bin/traffic-cop',
         'bin/tt-wrapper',
     ],
+    # Handling direct file installation here rather than with debian/install.
     data_files=[
-        # ('share/polkit-1/actions', glob.glob('data/actions/*.policy')),
         ('share/icons/hicolor/scalable/apps', glob.glob('data/icons/*.svg')),
         (f"share/{deb_pkg}/ui", glob.glob('data/ui/*.glade')),
         (f"share/{deb_pkg}", ['data/traffic-cop.yaml.default']),
