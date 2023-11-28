0ae544f3dfb559d8b0e9218546c53f73a59313aa
Nate Marti <nate_marti@sil.org>
2023-11-28 11:24:13 +0100

    store executable as array

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 file_path=$(readlink -f "$0")
-root_dir=$(dirname $(dirname "$file_path"))
+root_dir=$(dirname "$(dirname "$file_path")")
 cd "${root_dir}"
 
 usage="Usage: $0 [-h] | [-t] [-v]"
@@ -10,7 +10,7 @@ help="$usage
    -t   Run unittest only. Don't check coverage.
    -v   Give verbose output.
 "
-executable="python3 -m coverage run --branch"
+executable=("python3" "-m" "coverage" "run" "--branch")
 verbose=''
 missing=''
 
@@ -21,7 +21,7 @@ while getopts ":htv" o; do
             exit 0
             ;;
         t)
-            executable="python3"
+            executable=("python3")
             ;;
         v)
             verbose='-v'
@@ -36,11 +36,11 @@ done
 shift $((OPTIND-1))
 
 # Run tests.
-${executable[@]} -m unittest discover "$verbose" -s tests
+"${executable[@]}" -m unittest discover "$verbose" -s tests
 echo -e "\n"
 
 # Show report when coverage is run.
-if [[ "$executable" == "python3 -m coverage run --branch" ]]; then
+if [[ "${executable[*]}" == "python3 -m coverage run --branch" ]]; then
     if [[ "$missing" ]]; then
         python3 -m coverage report "$missing"
     else
