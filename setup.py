150f839e2f9d7b0535cc52a8a7973eb01ac39e06
Nate Marti <nate_marti@sil.org>
2023-11-29 09:52:34 +0100

    re-include polkit action file

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 # https://docs.python.org/3/distutils/setupscript.html
 
-import glob
 from setuptools import setup
-
-deb_pkg = 'traffic-cop'
 
 setup(
     scripts=[
@@ -11,10 +8,11 @@ setup(
     ],
     # Handling direct file installation here rather than with debian/install.
     data_files=[
-        ('share/icons/hicolor/scalable/apps', glob.glob('data/icons/*.svg')),
-        (f"share/{deb_pkg}/ui", glob.glob('data/ui/*.glade')),
-        (f"share/{deb_pkg}", ['data/traffic-cop.yaml.default']),
-        ('share/applications', glob.glob('data/applications/*.desktop')),
+        ('share/polkit-1/actions', ['data/actions/org.wasta.apps.traffic-cop.policy']),
+        ('share/icons/hicolor/scalable/apps', ['data/icons/traffic-cop.svg']),
+        ('share/traffic-cop/ui', ['data/ui/mainwindow.glade']),
+        ('share/traffic-cop', ['data/traffic-cop.yaml.default']),
+        ('share/applications', ['data/applications/traffic-cop.desktop']),
         ('lib/systemd/system-preset', ['data/traffic-cop.preset']),
     ]
 )
